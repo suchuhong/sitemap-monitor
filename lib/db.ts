f83951a4598881bb2fee9f9c682726d648bd216b
such <1442615457@qq.com>
2025-10-04 23:25:02 +0800

    fix: 调整数据库连接超时配置
    - 将连接超时从 2 秒增加到 10 秒
    - 验证数据库连接成功

--- lib/db.ts
+++ lib/db.ts
@@ -26,7 +26,7 @@ export function resolveDb(): DatabaseClient {
         connectionString,
         max: 20, // Maximum number of clients in the pool
         idleTimeoutMillis: 30000, // Close idle clients after 30 seconds
-        connectionTimeoutMillis: 2000, // Return an error after 2 seconds if connection could not be established
+        connectionTimeoutMillis: 10000, // Return an error after 10 seconds if connection could not be established
       });
     }
     
